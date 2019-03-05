44b0d5ad98d856c72926daa5343c4867f8d9ffac
pupongsin7 <nut38318@gmail.com>
2019-03-05 08:39:00 +0000

    add dolog.sh for Mid-Term No.7

---
+++ dolog.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+start=$1
+end=$2
+word=$3
+loop=$(date -d "$start" +%Y%m%d) || exit -1
+st=$(date -d "$start" +%Y-%m-%d) || exit -1
+en=$(date -d "$end" +%Y%m%d) || exit -1
+file=""
+while [ "$loop" -le "$en" ]; do
+	echo $st
+
+	file+=$(grep  ${st}.*${word} testlog)"\n"
+
+	loop=$(date -d "$loop + 1 day" +"%Y%m%d")
+	st=$(date -d "$loop" +"%Y-%m-%d")
+done
+echo $file >dolog.txt
